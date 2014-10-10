5c82f861dc714f776d214c7e79bfbcf4ca8f3e45
Harry <Harry@192.168.0.6>
2014-10-10 14:56:03 +0100

    Iteration R&R activities

---
+++ asking for message and number of reps.py
@@ -0,0 +1,3 @@
+#Harry Robinson
+#10-10-2014
+#Asking for message and umbers of repitions 
---
+++ iteration first program.py
@@ -0,0 +1,9 @@
+#Harry Robinson
+#10-10-2014
+#Iteration programme
+
+result = 0
+while result == 0:
+    number = int(input("Give a number: "))
+    result = result + number
+    print(result)
