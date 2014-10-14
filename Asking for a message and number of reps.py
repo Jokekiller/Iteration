2515db43f7b6bc098d28e63f4569a373ad0c4921
Harry <Harry@192.168.0.6>
2014-10-14 17:18:02 +0100

    Asking for a message ad number of reps

---
+++ Asking for a message and number of reps.py
@@ -0,0 +1,9 @@
+#Harry Robinson
+#14-10-2014
+#program stating a given message a certain number of times
+
+messageRepeated = int(input("Give the number if times you want the message to be repeated: "))
+message = input("Give your message: ")
+for messageRepeated in range(messageRepeated):
+    print(message)
+    
