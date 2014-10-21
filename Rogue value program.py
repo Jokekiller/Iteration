0c615f95c1d13811e6c76a06392388830f10e67d
Harry Robinson <41194@longroad.ac.uk>
2014-10-21 15:17:20 +0100

    Rogue value and positive integer

--- Rogue value program.py
+++ Rogue value program.py
@@ -4,15 +4,16 @@
 
 total = 0
 counter = 0
+finished = False
 
-amountOfNumbers = int(input("Put the amount of numbers you want to enter: "))
-while counter < amountOfNumbers:
-    number = float(input("Please enter {0} in the series you wish to be averaged.Enter -1 when you have finished.".format(counter)))
+
+while not finished:
+    number = float(input("Enter a number 'x'. Enter -1 when you have finished."))
     if number == -1:
-        print("You have ended the program")
+        finished = True
     else:
         print("The program will carry on.")
-    total = total + number
-    counter = counter + 1
-average = total / amountOfNumbers
+        total = total + number
+        counter = counter + 1
+average = total / counter
 print(average)
---
+++ conversion table.py
@@ -0,0 +1,10 @@
+#Harry Robinson
+#21-10-2014
+#Conversion table
+
+count = 0
+
+for count in range(1,21):
+    result = count / 2.2
+    print("{0:>2} lbs is {1:>2.2f} kg.".format(count, result))
+    
